8ecff9869d927a5a7047d5ed018bac6c52b614c1
Patric Pereira <patric.pereirafranca@gmail.com>
2019-04-19 16:21:34 -0300

    criando testes e UPDATE heroes

--- src/db/strategies/postgres.js
+++ src/db/strategies/postgres.js
@@ -65,6 +65,10 @@ class Postgres extends ICrud {
   async read(item = {}) {
     return this._heroes.findAll({ where: item, raw: true });
   }
+
+  async update(id, item) {
+    return this._heroes.update(item, { where: { id: id } });
+  }
 }
 
 module.exports = Postgres;
--- src/tests/postgresStrategy.test.js
+++ src/tests/postgresStrategy.test.js
@@ -3,12 +3,14 @@ const Postgres = require('../db/strategies/postgres');
 const Context = require('../db/strategies/base/contextStrategy');
 
 const context = new Context(new Postgres());
-const MOCK_HEROES_CADASTRAR = { nome: 'Gavião Negro', poder: 'Flexas' }
+const MOCK_HEROES_CADASTRAR = { nome: 'Gavião Negro', poder: 'Flexas' };
+const MOCK_HEROES_ATUALIZAR = { nome: 'Batman', poder: 'Dinheiro' }
 
 describe('Postgres Strategy', function () {
   this.timeout(Infinity);
   this.beforeAll(async () => {
     await context.connect();
+    await context.create(MOCK_HEROES_ATUALIZAR);
   })
 
   it('Postgres SQL Connection', async () => {
@@ -26,5 +28,18 @@ describe('Postgres Strategy', function () {
     const [result] = await context.read({ nome: MOCK_HEROES_CADASTRAR.nome });
     delete result.id;
     assert.deepEqual(result, MOCK_HEROES_CADASTRAR);
+  });
+
+  it('atualizar', async () => {
+    const [itemAtualizar] = await context.read({ nome: MOCK_HEROES_ATUALIZAR.nome });
+    const novoItem = {
+      ...MOCK_HEROES_ATUALIZAR,
+      nome: 'Mulher Maravilha'
+    }
+    const [result] = await context.update(itemAtualizar.id, novoItem);
+    const [itemAtualizado] = await context.read({ id: itemAtualizar.id });
+
+    assert.deepEqual(result, 1);
+    assert.deepEqual(itemAtualizado.nome, novoItem.nome);
   });
 });
